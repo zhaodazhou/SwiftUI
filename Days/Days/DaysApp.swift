3a5cf3ae822a66d3425f4ffe7314c394ba5c8350
Summer <zhaodazhou@yeah.net>
2023-06-09 16:41:15 +0800

    Add files via upload
    100 Days of SwiftUI

---
+++ Days/Days/DaysApp.swift
@@ -0,0 +1,18 @@
+//
+//  DaysApp.swift
+//  Days
+//
+//  Created by dazhou on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            SwiftUIView()
+        }
+    }
+}
